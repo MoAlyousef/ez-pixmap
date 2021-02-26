a6b1617e796d488c4017c3ce4afad049587246ec
MoAlyousef <may642_2000@hotmail.com>
2021-02-26 13:04:45 +0300

    add more examples

---
+++ examples/fltk.rs
@@ -0,0 +1,60 @@
+extern crate ez_pixmap;
+extern crate fltk;
+
+use fltk::*;
+
+const PXM: &[&str] = &[
+    "50 34 4 1",
+    "  c black",
+    "o c #ff9900",
+    "@ c white",
+    "# c None",
+    "##################################################",
+    "###      ##############################       ####",
+    "### ooooo  ###########################  ooooo ####",
+    "### oo  oo  #########################  oo  oo ####",
+    "### oo   oo  #######################  oo   oo ####",
+    "### oo    oo  #####################  oo    oo ####",
+    "### oo     oo  ###################  oo     oo ####",
+    "### oo      oo                     oo      oo ####",
+    "### oo       oo  ooooooooooooooo  oo       oo ####",
+    "### oo        ooooooooooooooooooooo        oo ####",
+    "### oo     ooooooooooooooooooooooooooo    ooo ####",
+    "#### oo   ooooooo ooooooooooooo ooooooo   oo #####",
+    "####  oo oooooooo ooooooooooooo oooooooo oo  #####",
+    "##### oo oooooooo ooooooooooooo oooooooo oo ######",
+    "#####  o ooooooooooooooooooooooooooooooo o  ######",
+    "###### ooooooooooooooooooooooooooooooooooo #######",
+    "##### ooooooooo     ooooooooo     ooooooooo ######",
+    "##### oooooooo  @@@  ooooooo  @@@  oooooooo ######",
+    "##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######",
+    "##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######",
+    "##### oooooooo  @@@  ooooooo  @@@  oooooooo ######",
+    "##### ooooooooo     ooooooooo     ooooooooo ######",
+    "###### oooooooooooooo       oooooooooooooo #######",
+    "###### oooooooo@@@@@@@     @@@@@@@oooooooo #######",
+    "###### ooooooo@@@@@@@@@   @@@@@@@@@ooooooo #######",
+    "####### ooooo@@@@@@@@@@@ @@@@@@@@@@@ooooo ########",
+    "######### oo@@@@@@@@@@@@ @@@@@@@@@@@@oo ##########",
+    "########## o@@@@@@ @@@@@ @@@@@ @@@@@@o ###########",
+    "########### @@@@@@@     @     @@@@@@@ ############",
+    "############  @@@@@@@@@@@@@@@@@@@@@  #############",
+    "##############  @@@@@@@@@@@@@@@@@  ###############",
+    "################    @@@@@@@@@    #################",
+    "####################         #####################",
+    "##################################################",
+];
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let my_image = ez_pixmap::RgbaImage::from(PXM)?;
+    let app = app::App::default();
+    let mut win = window::OverlayWindow::default().with_size(50, 34);
+    let mut frame = frame::Frame::default().size_of(&win);
+    win.end();
+    win.show();
+    frame.draw(move || {
+        draw::draw_image(my_image.data(), 0, 0, 50, 34, ColorDepth::Rgba8).unwrap();
+    });
+    app.run().unwrap();
+    Ok(())
+}
---
+++ examples/ufb.rs
@@ -0,0 +1,51 @@
+extern crate ez_pixmap;
+extern crate ufb;
+
+const PXM: &[&str] = &[
+    "50 34 4 1",
+    "  c black",
+    "o c #ff9900",
+    "@ c white",
+    "# c None",
+    "##################################################",
+    "###      ##############################       ####",
+    "### ooooo  ###########################  ooooo ####",
+    "### oo  oo  #########################  oo  oo ####",
+    "### oo   oo  #######################  oo   oo ####",
+    "### oo    oo  #####################  oo    oo ####",
+    "### oo     oo  ###################  oo     oo ####",
+    "### oo      oo                     oo      oo ####",
+    "### oo       oo  ooooooooooooooo  oo       oo ####",
+    "### oo        ooooooooooooooooooooo        oo ####",
+    "### oo     ooooooooooooooooooooooooooo    ooo ####",
+    "#### oo   ooooooo ooooooooooooo ooooooo   oo #####",
+    "####  oo oooooooo ooooooooooooo oooooooo oo  #####",
+    "##### oo oooooooo ooooooooooooo oooooooo oo ######",
+    "#####  o ooooooooooooooooooooooooooooooo o  ######",
+    "###### ooooooooooooooooooooooooooooooooooo #######",
+    "##### ooooooooo     ooooooooo     ooooooooo ######",
+    "##### oooooooo  @@@  ooooooo  @@@  oooooooo ######",
+    "##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######",
+    "##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######",
+    "##### oooooooo  @@@  ooooooo  @@@  oooooooo ######",
+    "##### ooooooooo     ooooooooo     ooooooooo ######",
+    "###### oooooooooooooo       oooooooooooooo #######",
+    "###### oooooooo@@@@@@@     @@@@@@@oooooooo #######",
+    "###### ooooooo@@@@@@@@@   @@@@@@@@@ooooooo #######",
+    "####### ooooo@@@@@@@@@@@ @@@@@@@@@@@ooooo ########",
+    "######### oo@@@@@@@@@@@@ @@@@@@@@@@@@oo ##########",
+    "########## o@@@@@@ @@@@@ @@@@@ @@@@@@o ###########",
+    "########### @@@@@@@     @     @@@@@@@ ############",
+    "############  @@@@@@@@@@@@@@@@@@@@@  #############",
+    "##############  @@@@@@@@@@@@@@@@@  ###############",
+    "################    @@@@@@@@@    #################",
+    "####################         #####################",
+    "##################################################",
+];
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let my_image = ez_pixmap::RgbaImage::from(PXM)?;
+    let mut win = ufb::Window::new(50, 34, "")?;
+    win.show(my_image.data(), ufb::ColorDepth::Rgba8)?;
+    Ok(())
+}
